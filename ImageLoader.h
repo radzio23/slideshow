b8bfda4ca0c5e7341b152d23f1553494c4e3ae16
vimstra <156113726+vimstra@users.noreply.github.com>
2025-12-13 18:14:07 +0100

    Add ImageLoader header for scanning image files

---
+++ ImageLoader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <filesystem>
+
+// Przestrzeń nazw dla funkcji narzędziowych.
+namespace ImageLoader
+{
+    // Skanuje podany katalog w poszukiwaniu plików graficznych.
+    std::vector<std::filesystem::path> scanDirectory(const std::string &path);
+}
